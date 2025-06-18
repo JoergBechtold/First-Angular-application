9c5f50f3c125c6460220d814586020299fb7a1f0
Jörg Bechtold <joergbechtold@gmx.net>
2025-06-18 10:53:35 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FriutlistComponent } from './friutlist/friutlist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+        {path: '', component: FriutlistComponent},
+    
+];
