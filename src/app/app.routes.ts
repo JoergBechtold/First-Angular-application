dd0adb1f4f9571a238958a5f59631c7795ec7d6b
Jörg Bechtold <joergbechtold@gmx.net>
2025-06-18 18:02:56 +0200

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { FriutlistComponent } from './friutlist/friutlist.component';
 
 export const routes: Routes = [
-    
-        {path: '', component: FriutlistComponent},
-    
+
+        { path: '', component: FriutlistComponent },
+
 ];
